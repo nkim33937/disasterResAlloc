297e3e50339d6db97b2eba1aa236595d1b18dcd8
nkim33937 <n33937@berkeley.edu>
2024-10-19 16:48:03 -0700

    moved data input

--- disasterResAlloc/disasterResAlloc.py
+++ disasterResAlloc/disasterResAlloc.py
@@ -14,3 +14,13 @@ app = rx.App(
     title="Disaster Relief Payment Gateway",
     description="A dashboard for fast payments for disaster relief.",
 )
+
+org_1 = Organisation(name="Red Cross Gaza", location="Gaza", email="redcrossgaza@gmail.com", encoded_wallet="0x1234567890")
+org_2 = Organisation(name="Amnesty International Gaza", location="Gaza", email="amnestygaza@gmail.com", encoded_wallet="0x1234567890")
+
+with rx.session() as session:
+    session.add(org_1)
+    session.add(org_2)
+    session.commit()
+    session.refresh(org_1)
+    session.refresh(org_2)
